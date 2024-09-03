6dab52f262aff8429057badbecc6c4f7501c59df
jfercher <johnathanfercher22@gmail.com>
2024-09-03 18:52:10 -0300

    add frontend

---
+++ neo4j/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Graph msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Graph from './components/Graph.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Graph,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ neo4j/frontend/src/components/Graph.vue
@@ -0,0 +1,40 @@
+<style scoped>
+
+</style>
+
+<template>
+  <div>
+    {{ graph }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      graph: null,
+    }
+  },
+  async mounted() {
+    await this.getGraph()
+  },
+  methods: {
+    async getGraph() {
+      this.graph = await axios.get("http://localhost:3000/graph").then(response => {
+        return response.data
+      }).catch(err => {
+        return err
+      })
+    }
+  }
+}
+</script>
+
+
+
